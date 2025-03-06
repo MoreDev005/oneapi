de7652ceab51c5a337130f90d3000d696c9126d3
Rurizora <bottoramku@gmail.com>
2025-03-06 18:34:17 +0700

    Update tube.js

--- tube.js
+++ tube.js
@@ -3,11 +3,10 @@ const regexYT = /(?<=[=\/&\%3D&])[a-zA-Z0-9_\-]{11}(?=[=\/&?#%\n\r]|$)/
 try{
 let videoID = regexYT.exec(url)[0]
 let urlyt = `https://www.youtube.com/watch?v=${videoID}`
-const ytdownload = require("@vreden/youtube_scraper")
-let linknya = await ytdownload.ytmp3(urlyt,'128')
-return {status: "sukses", author: "iwan", result: {link:linknya.download.url,linkUnblock:`${baseUrl}/download/id?data=${encodeURIComponent(linknya.download.url)}`}}
-//return {status: "sukses", author: "iwan", result: {link:`${baseUrl}/ytdlmp3/id?data=${encodeURIComponent(videoID)}`,linkUnblock:`${baseUrl}/ytdlmp3/id?data=${encodeURIComponent(videoID)}`}}
-
+//const ytdownload = require('./newyt.js')
+//let linknya = await ytdownload(urlyt)
+//return {status: "sukses", author: "iwan", result: {link:`${baseUrl}/download/id?data=${encodeURIComponent(linknya.link)}`,linkUnblock:`${baseUrl}/download/id?data=${encodeURIComponent(linknya.link)}`}}
+return {status: "sukses", author: "iwan", result: {link:`${baseUrl}/ytdlmp3/id?data=${encodeURIComponent(videoID)}`,linkUnblock:`${baseUrl}/ytdlmp3/id?data=${encodeURIComponent(videoID)}`}}
 }catch(e){
 return {status: "error", author: "iwan", message: "Error, Mungkin Api Rusak (code 1)"}
 }
